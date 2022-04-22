a7115eaeef6d3838a09e5a6eb71dc01b95464038
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-04-22 11:40:09 +0530

    #include<stdio.h> int main() {     char expression[50];     int x=0, i=0;     scanf("%s", expression);  while(expression[i]!= NULL)     {
         if(expression[i]=='(')  
            {  
                x++;  
            }  
           
         else if(expression[i]==')')  
            {  
                x--;  
                if(x<0)  
                break;  
            }  
        i++;        
        }  
          
        if(x==0)  
        {  
            printf("True");  
        }  
          
        else  
        {  
            printf("False");  
        }  
        return 0;  
    }

---
+++ Balanced_Parentheses_1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>  
+int main()  
+{  
+    char expression[50];  
+    int x=0, i=0; 
+    scanf("%s", expression);  
+ while(expression[i]!= NULL)  
+    {  
+        
+     if(expression[i]=='(')  
+        {  
+            x++;  
+        }  
+       
+     else if(expression[i]==')')  
+        {  
+            x--;  
+            if(x<0)  
+            break;  
+        }  
+    i++;        
+    }  
+      
+    if(x==0)  
+    {  
+        printf("True");  
+    }  
+      
+    else  
+    {  
+        printf("False");  
+    }  
+    return 0;  
+}
