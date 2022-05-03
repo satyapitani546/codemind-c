8c9f72100698e77245e5ddf3d443c063d282345e
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-05-03 23:13:35 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,r,s=0,c=0,n1,n2;     scanf("%d",&n);     n1=n2=n;     c=(n==0)?1:log10(n)+1;     while(n1>0)     {         r=n1%10;         s=s+pow(r,c);         n1=n1/10;         c--;     }     if(n2==s)     printf("True");     else     printf("False"); }

--- Disarium_number.c
+++ Disarium_number.c
@@ -5,7 +5,7 @@ int main()
     int n,i,r,s=0,c=0,n1,n2;
     scanf("%d",&n);
     n1=n2=n;
-    c=(n==0)?1:log10(n)+1; 
+    c=(n==0)?1:log10(n)+1;
     while(n1>0)
     {
         r=n1%10;
@@ -14,7 +14,7 @@ int main()
         c--;
     }
     if(n2==s)
-     printf("True");
+    printf("True");
     else
-     printf("False");
+    printf("False");
 }
