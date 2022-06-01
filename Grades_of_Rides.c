2aeb2ad89bb0c6c40ac02115e32172edf58f4d6b
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-06-01 14:51:48 +0530

    #include<stdio.h> int main() {     int hf,sf,df;     scanf("%d%d%d",&hf,&sf,&df);     if(hf>50 && sf>60 && df>100)     {         printf("10");     }     else if(hf>50 && sf>60)     {         printf("9");     }     else if(sf>60 && df>100)     {         printf("8");     }     else if(hf>50 && df>100)     {         printf("7");     }     else if(hf>50 || sf>60 || df>100)     {         printf("6");     }     else     {         printf("5");     } }

---
+++ Grades_of_Rides.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int hf,sf,df;
+    scanf("%d%d%d",&hf,&sf,&df);
+    if(hf>50 && sf>60 && df>100)
+    {
+        printf("10");
+    }
+    else if(hf>50 && sf>60)
+    {
+        printf("9");
+    }
+    else if(sf>60 && df>100)
+    {
+        printf("8");
+    }
+    else if(hf>50 && df>100)
+    {
+        printf("7");
+    }
+    else if(hf>50 || sf>60 || df>100)
+    {
+        printf("6");
+    }
+    else
+    {
+        printf("5");
+    }
+}
