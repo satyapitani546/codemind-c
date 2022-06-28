edffa39c4b0007d9a6a6cc0a3615af90439c3331
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-06-28 21:01:23 +0530

    #include<stdio.h> int main() {     int n,a[n],f=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0 & i%2!=0)         {             f=1;         }     }     if(f==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[n],f=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0 & i%2!=0)
+        {
+            f=1;
+        }
+    }
+    if(f==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
