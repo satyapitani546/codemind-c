2c0d582701589dffd103c1242fbdf8fe5b5870e7
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-07-02 20:41:27 +0530

    #include<stdio.h> int main() {     int n,i,j,a[100],b[100],c=0,k=0,m,max;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(a[i]==c)         {             b[k]=a[i];             k++;         }     }     if(k==0)     {         printf("-1");         return 0;     }     for(i=0;i<k;i++)     {         for(j=0;j<k;j++)         {             if(b[i]==b[j]&&i!=j)             {                 b[j]=-1;             }         }     }     for(i=0;i<k;i++)     {         if(b[i]!=-1)         {             m=b[0];             if(m<b[i])             {                 m=b[i];             }             else             {                 max=b[i];             }         }     }     printf("%d %d",max,m); }

---
+++ Maximum_and_Minimum.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,a[100],b[100],c=0,k=0,m,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(a[i]==c)
+        {
+            b[k]=a[i];
+            k++;
+        }
+    }
+    if(k==0)
+    {
+        printf("-1");
+        return 0;
+    }
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            if(b[i]==b[j]&&i!=j)
+            {
+                b[j]=-1;
+            }
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        if(b[i]!=-1)
+        {
+            m=b[0];
+            if(m<b[i])
+            {
+                m=b[i];
+            }
+            else
+            {
+                max=b[i];
+            }
+        }
+    }
+    printf("%d %d",max,m);
+}
