88eafcc82f42797da72e4dcd2f922593fd9a54b8
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-07-08 20:00:46 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],a,b,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             printf("%d ",arr[i]);             c++;         }     }     if(c==0)     {         printf("-1");     } }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],a,b,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            printf("%d ",arr[i]);
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+}
