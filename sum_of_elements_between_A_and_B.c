a996d2a9ffc036ea8b57a65c6543f4f10e93d62d
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-07-08 19:50:30 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],s=0,a,b;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             s=s+arr[i];         }     }     printf("%d",s); }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],s=0,a,b;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            s=s+arr[i];
+        }
+    }
+    printf("%d",s);
+}
